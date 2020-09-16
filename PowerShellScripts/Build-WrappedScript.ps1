a292d83824eda5244f8bea7548e3b339ff27637c
John Hofmann <John.Hofmann@charter.com>
2020-09-16 09:52:17 -0400

    Added license information
    - Added GPL3 License boilerplate

--- PowerShellScripts/Build-WrappedScript.ps1
+++ PowerShellScripts/Build-WrappedScript.ps1
@@ -6,6 +6,17 @@
 									#														Date		09/15/2020																#
 									#																																			#
 									#											Copyright © 2020 John Hofmann All Rights Reserved												#
+									#											https://github.com/John-Hofmann/HofmanniaStudios												#
+									#																																			#
+									#									This program is free software: you can redistribute it and/or modify									#
+									#									it under the terms of the GNU General Public License as published by									#
+									#										the Free Software Foundation, either version 3 of the License, or									#
+									#													(at your option) any later version.														#
+									#																																			#
+									#										This program is distributed in the hope that it will be useful,										#
+									#										but WITHOUT ANY WARRANTY; without even the implied warranty of										#
+									#										MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the										#
+									#												GNU General Public License for more details.												#
 									#																																			#
 									#═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════#
 									#															Changelog																		#
