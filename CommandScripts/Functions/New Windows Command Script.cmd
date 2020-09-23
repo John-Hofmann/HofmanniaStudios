383d472ebc7abee2b8305c8b27c2f03ac5e5e89e
John Hofmann <John.Hofmann@charter.com>
2020-09-23 15:49:22 -0400

    Initializing CSFunctionsRoot

---
+++ CommandScripts/Functions/New Windows Command Script.cmd
@@ -0,0 +1,39 @@
+									::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+									::																																			::
+									::														FileName	FileName																::
+									::														Author		John Hofmann															::
+									::														Version		0.0.1																	::
+									::														Date		MM/DD/YYYY																::
+									::																																			::
+									::											Copyright © 2020 John Hofmann All Rights Reserved												::
+									::											https://github.com/John-Hofmann/HofmanniaStudios												::
+									::																																			::
+									::									This program is free software: you can redistribute it and/or modify									::
+									::									it under the terms of the GNU General Public License as published by									::
+									::										the Free Software Foundation, either version 3 of the License, or									::
+									::													(at your option) any later version.														::
+									::																																			::
+									::										This program is distributed in the hope that it will be useful,										::
+									::										but WITHOUT ANY WARRANTY; without even the implied warranty of										::
+									::										MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the										::
+									::												GNU General Public License for more details.												::
+									::																																			::
+									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
+									::															Changelog																		::
+									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
+									::	Date			Version		Notes																										::
+									::	──────────		───────		─────────────────────────────────────────────────────────────────────────────────────────────────────────── ::
+									::	MM/DD/YYYY		0.0.1		Initial Build																								::
+									::																																			::
+									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
+									::														  Known Issues																		::
+									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
+									::	None																																	::
+									::																																			::
+									::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+<PLACE SCRIPT HERE>
+EXIT /B 0
+
+:**FUNCTIONS**
+<PLACE FUNCTIONS HERE>
