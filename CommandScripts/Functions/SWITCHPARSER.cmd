1049022768baa797bcc6a1444bdd6fe9333cfea8
John Hofmann <John.Hofmann@charter.com>
2020-09-23 22:43:41 -0400

    Initial build

--- CommandScripts/Functions/SWITCHPARSER.cmd
+++ CommandScripts/Functions/SWITCHPARSER.cmd
@@ -1,9 +1,9 @@
 									::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 									::																																			::
-									::														FileName	FileName																::
+									::														FileName	SWITCHPARSER.cmd																::
 									::														Author		John Hofmann															::
 									::														Version		0.0.1																	::
-									::														Date		MM/DD/YYYY																::
+									::														Date		09/23/2020																::
 									::																																			::
 									::											Copyright © 2020 John Hofmann All Rights Reserved												::
 									::											https://github.com/John-Hofmann/HofmanniaStudios												::
@@ -23,7 +23,7 @@
 									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
 									::	Date			Version		Notes																										::
 									::	──────────		───────		─────────────────────────────────────────────────────────────────────────────────────────────────────────── ::
-									::	MM/DD/YYYY		0.0.1		Initial Build																								::
+									::	09/23/2020		0.0.1		Initial Build																								::
 									::																																			::
 									::══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════::
 									::														  Known Issues																		::
@@ -32,8 +32,104 @@
 									::																																			::
 									::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-<PLACE SCRIPT HERE>
-EXIT /B 0
+::NAME
+::    SCRIPTNAME
+::    <-4 SPACES    Do not exceed 73 columns to prevent text wrapping.->|
+::
+::SYNOPSIS
+::    Synopsis                                                          |
+::
+::
+::SYNTAX
+::    Syntax                                                            |
+::
+::
+::DESCRIPTION
+::    Description                                                       |
+::
+::
+::PARAMETERS
+::    -PARAMETER1                                                       |
+::        Description.
+::        <-8 SPACES Don't exceed 73 columns to prevent text wrapping.->|
+::
+::    -PARAMETER2                                                       |
+::        Description.                                                  |
+::
+:ENDOFPARAMETERS
+::INPUTS
+::    Type of input                                                     |
+::
+::
+::        Description of input.                                         |
+::
+::
+::OUTPUTS
+::    Type of output                                                    |
+::
+::
+::        Description of output.                                        |
+::
+::
+::EXIT CODES
+::    %ERRORLEVEL% = Description of exit code.                          |
+::
+::
+::NOTES
+::
+::
+::        NOTES                                                         |
+::
+::
+::    -------------------------- EXAMPLE 1 --------------------------
+::    <-4 SPACES       Do not exceed 69 columns to keep alignment.->|
+::    EXAMPLE
+:ENDOFHELP
+
+@ECHO OFF
+SET "DEBUG=REM "
+IF /I "%~1" EQU "-DEBUG" (
+	SHIFT /1
+	SET "DEBUG=ECHO:"
+)
+SETLOCAL ENABLEDELAYEDEXPANSION
+	%DEBUG%Setting FILE...
+	SET "FILE="%~f0""
+	%DEBUG%Setting SKIP...
+	SET "SKIP="
+	FOR /F "delims=:" %%A IN ('FINDSTR /B /N "::PARAMETERS" %FILE%') DO (
+		%DEBUG%Searching for ::PARAMETERS...
+		SET /A "SKIP=%%A-2"
+		GOTO :BREAK1
+	)
+	:BREAK1
+	%DEBUG%:BREAK1
+	IF NOT DEFINED SKIP (
+		%DEBUG%SKIP not defined...
+		EXIT /B 5926
+	)
+	%DEBUG%Setting PARAMETERCOUNT...
+	SET "PARAMETERCOUNT=0"
+	%DEBUG%Setting PARAMETERS...
+	SET "PARAMETERS="
+	FOR /F "usebackq skip=%SKIP% delims=" %%A IN (%FILE%) DO (
+		%DEBUG%Searching for parameters...
+		IF "%%A" EQU ":ENDOFPARAMETERS" (
+			%DEBUG%ENDOFPARAMETERS...
+			ECHO:!PARAMETERS!
+			ECHO:!PARAMETERCOUNT!
+			EXIT /B 0
+		)
+		SET "LINE=%%A"
+		SET "LINE=!LINE:|=!"
+		SET "LINE=!LINE:<=!"
+		SET "LINE=!LINE:>=!"
+		FOR /F "delims=: " %%B IN ('ECHO:!LINE! ^| FINDSTR /I /C:"::    -"') DO (
+			SET "PARAMETERS=!PARAMETERS!%%B;"
+			SET /A "PARAMETERCOUNT += 1"
+		)
+	)
+EXIT /B 1
 
 :**FUNCTIONS**
 <PLACE FUNCTIONS HERE>
